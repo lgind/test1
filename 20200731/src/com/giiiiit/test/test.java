ab8e2453cd670bc564bfe72105f92fc90f432801
Lgind <1144714911@qq.com>
2020-07-31 09:25:39 +0800

    V1.1

--- 20200731/src/com/giiiiit/test/test.java
+++ 20200731/src/com/giiiiit/test/test.java
@@ -2,6 +2,8 @@ package com.giiiiit.test;
 
 public class test {
 	public static void main(String[] args) {
-		System.out.println("hallo world");
+		System.out.println("hello world");
+		int a=100;
+		System.out.println(a);
 	}
 }
